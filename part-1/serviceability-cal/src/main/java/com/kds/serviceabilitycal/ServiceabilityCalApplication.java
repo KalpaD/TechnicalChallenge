a48f27127f00dc81ef82bcd4a6b3d9313dbf39f4
Kalpa Senanayake <senanayake.kalpa@gmail.com>
2021-03-17 07:40:38 +1100

    Adding skeleton of the part-1

---
+++ part-1/serviceability-cal/src/main/java/com/kds/serviceabilitycal/ServiceabilityCalApplication.java
@@ -0,0 +1,13 @@
+package com.kds.serviceabilitycal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceabilityCalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceabilityCalApplication.class, args);
+	}
+
+}
